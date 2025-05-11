bd8fb5d6244d3952a592a971b24a5891e72f3608
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-05-11 19:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+
+function ignoreChar(str) {
+		return str.replace(/^(A |An |The)/i,"").trim();
+}
+bands.sort((a,b)=>ignoreChar(a)>ignoreChar(b)?1:-1);
+
+
+const mylist=document.querySelector("#band")
+
+mylist.innerHTML=bands.map(band => `<li> ${band}<li>`).join("");
+mylist.style.list-style;
+
